74ddcb87b6d3dd007b85bddce486249f49961ef7
jordan381 <iamastro66@gmail.com>
2024-10-09 14:40:40 -0400

    Update fstock.py

--- cogs/fstock.py
+++ cogs/fstock.py
@@ -15,7 +15,7 @@ class fstock(commands.Cog):
     @bot.slash_command(name="fstock", description="📜 Display the list of our account stock.")
     async def fstock(self, ctx):
         embed = nextcord.Embed(title="📜 Free Account Stock:", timestamp=datetime.utcnow(), color=nextcord.Colour.green())
-        embed.set_footer(text="Free Version: https://github.com/zkoolmao/Account-Generator-Bot", icon_url="https://cdn.discordapp.com/attachments/1204244617869000788/1204249308237402162/icon.png?ex=65d40b90&is=65c19690&hm=42115bccb811f86d7a9cdbcb686a2f1c2a5be6633e04f712da3765d6da475e03&")
+        embed.set_footer(text="Stock Loading..", icon_url="https://cdn.discordapp.com/attachments/1204244617869000788/1204249308237402162/icon.png?ex=65d40b90&is=65c19690&hm=42115bccb811f86d7a9cdbcb686a2f1c2a5be6633e04f712da3765d6da475e03&")
         embed.timestamp = datetime.now()
         embed.description = ""
         for filename in os.listdir("fstock/"):
